75384ea150e178cff16e8d827c3522928934abc6
nafkhanzam <nafkhanalzamzami@gmail.com>
2020-10-14 16:16:28 +0700

    add image to work experiences

--- src/pages/_components/Content.svelte
+++ src/pages/_components/Content.svelte
@@ -12,17 +12,27 @@
   <h2 class="mb-4 font-weight-bold">{item.title}</h2>
   <div style="margin-left: 3em;">
     {#each item.contents || [] as content}
-      <div class="d-flex justify-content-between">
-        <div>
-          <h4 class="font-weight-bold">{content.title}</h4>
-          <p>
-            {@html content.desc}
-          </p>
+      <div class="d-flex justify-content-between mt-4">
+        <div class="d-flex">
+          {#if content.img}
+            <img
+              src={content.img}
+              alt={content.title}
+              style="height: 100%; width: 100px;" />
+            <div style="width: 32px;" />
+          {/if}
+          <div>
+            <h4 class="font-weight-bold">{content.title}</h4>
+            <p>
+              {@html content.desc}
+            </p>
+          </div>
         </div>
         <div>
           <p>{content.time}</p>
         </div>
       </div>
+      <hr />
     {:else}
       <slot />
     {/each}
--- src/pages/portfolio.svelte
+++ src/pages/portfolio.svelte
@@ -5,30 +5,43 @@
     title: "WORK EXPERIENCE",
     contents: [
       {
-        title: "Alinamed",
-        desc: "Lead Project Engineer - Full time",
-        time: "June 2020 - Present"
+        img: "https://d2w51pw0y636wu.cloudfront.net/assets/images/favicon.png",
+        title: "AlinaMed",
+        desc: "Chief Technology Officer - Full time",
+        time: "August 2020 - Present",
       },
       {
-        title: "Impulse ITB",
+        img:
+          "https://media-exp1.licdn.com/dms/image/C560BAQFqOnrL1XfH1w/company-logo_200_200/0?e=1610582400&v=beta&t=xcLAOf871zMiqC6wdIB_Jrn_NsdkCS7I4QPYGKDiMFY",
+        title: "Impulse ITB 2020",
         desc: "Back End Developer - Full time",
-        time: "July 2020 - Present"
+        time: "July 2020 - Present",
       },
       {
+        img: "https://d2w51pw0y636wu.cloudfront.net/assets/images/favicon.png",
+        title: "AlinaMed",
+        desc: "Lead Project Engineer - Full time",
+        time: "June 2020 - August 2020",
+      },
+      {
+        img:
+          "https://media-exp1.licdn.com/dms/image/C4D0BAQHpXhPivbZyzA/company-logo_200_200/0?e=1610582400&v=beta&t=n6dsei6TVZinMQUofCdmK3_UM3In-Z5xxlnbwayQn5w",
         title: "GDP Labs",
         desc: "Software Development Engineer - Internship",
-        time: "June 2020 - August 2020"
+        time: "June 2020 - August 2020",
       },
       {
+        img:
+          "https://media-exp1.licdn.com/dms/image/C560BAQGZUTKmp-2Jjw/company-logo_200_200/0?e=1610582400&v=beta&t=qJdWnDQ93WETK4u7EPUT_jAokn_X18eZ211BbZ6cr1Q",
         title: "Xtremax",
         desc: "Back End Developer - Part time",
-        time: "August 2019 - February 2020"
-      }
-    ]
+        time: "August 2019 - February 2020",
+      },
+    ],
   };
 
   const skillExperience = {
-    title: "SKILL EXPERIENCE"
+    title: "SKILL EXPERIENCE",
   };
 
   const education = {
@@ -37,10 +50,10 @@
       {
         title: "INSTITUT TEKNOLOGI BANDUNG",
         desc:
-          "Computer Science - Bachelor's degree<br />Currently on the 4th semester with the current GPA 3.76/4.0",
-        time: "August 2018 - Present"
-      }
-    ]
+          "Computer Science - Bachelor's degree<br />Currently on the 5th semester with the current GPA 3.76/4.0",
+        time: "August 2018 - Present",
+      },
+    ],
   };
 
   const achievement = {
@@ -49,14 +62,14 @@
       {
         title: "ARKAVIDIA 5.0 COMPETITIVE PROGRAMMING",
         desc: "Finalist",
-        time: "2019"
+        time: "2019",
       },
       {
         title: "INTERNATIONAL COLLEGIATE PROGRAMMING CONTEST",
         desc: "Regional Finalist",
-        time: "2018"
-      }
-    ]
+        time: "2018",
+      },
+    ],
   };
 </script>
 
@@ -80,7 +93,8 @@
     <h1 class="font-weight-bold">MOCH. NAFKHAN ALZAMZAMI</h1>
     <h4>Computer Science Student</h4>
     <p class="text-justify">
-      A 4<sup>th</sup> semester undergraduate computer science student that's passionate in
+      A 5<sup>th</sup>
+      semester undergraduate computer science student that's passionate in
       software engineering, computer science, and any other IT related. Learning
       by trial and error method by teaching, creating projects, and solving
       problems. Easy to adapt in new environments.
@@ -104,21 +118,12 @@
   <p>Java, C#, Javascript, Typescript, C/C++, Python, Go, Rust</p>
   <h4 class="font-weight-bold mt-4">WEB DEVELOPMENT</h4>
   <ul>
-    <li>
-      <b>Backend:</b>
-      Node.js, .NET, Java, GraphQL
-    </li>
-    <li>
-      <b>Frontend:</b>
-      React, React-Native, Vue, Svelte, Flutter
-    </li>
-    <li>
-      <b>Cloud:</b>
-      Git, AWS, Docker, Firebase
-    </li>
+    <li><b>Backend:</b> Node.js, .NET, Java, GraphQL</li>
+    <li><b>Frontend:</b> React, React-Native, Vue, Svelte, Flutter</li>
+    <li><b>Cloud:</b> Git, AWS, Docker, Firebase</li>
     <li>
       <b>Database:</b>
-      MongoDB, SQL Server, MySQL, FaunaDB, DynamoDB, Firestore
+      MongoDB, SQL Server, MySQL, FaunaDB, DynamoDB, Firestore, Postgresql
     </li>
   </ul>
   <h4 class="font-weight-bold">OTHERS</h4>
